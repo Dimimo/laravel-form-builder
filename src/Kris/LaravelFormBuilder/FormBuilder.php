7203a75f385071797d5c0cc65480abdc2cc8f5c2
Dimitri Mostrey <dmostrey@yahoo.com>
2020-09-14 14:15:45 +0800

    PHPdoc fix FormBuilder.php

--- src/Kris/LaravelFormBuilder/FormBuilder.php
+++ src/Kris/LaravelFormBuilder/FormBuilder.php
@@ -2,8 +2,11 @@
 
 namespace Kris\LaravelFormBuilder;
 
+use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
+use InvalidArgumentException;
 use Kris\LaravelFormBuilder\Events\AfterFormCreation;
 
 class FormBuilder
@@ -56,17 +59,19 @@ class FormBuilder
     /**
      * Create a Form instance.
      *
-     * @param string $formClass The name of the class that inherits \Kris\LaravelFormBuilder\Form.
-     * @param array $options|null
-     * @param array $data|null
+     * @param string  $formClass The name of the class that inherits \Kris\LaravelFormBuilder\Form.
+     * @param array  $options |null
+     * @param array  $data |null
      * @return Form
+     * @throws BindingResolutionException
+     * @throws Exception
      */
     public function create($formClass, array $options = [], array $data = [])
     {
         $class = $this->getNamespaceFromConfig() . $formClass;
 
         if (!class_exists($class)) {
-            throw new \InvalidArgumentException(
+            throw new InvalidArgumentException(
                 'Form class with name ' . $class . ' does not exist.'
             );
         }
@@ -84,9 +89,11 @@ class FormBuilder
 
     /**
      * @param $items
-     * @param array $options
-     * @param array $data
+     * @param array  $options
+     * @param array  $data
      * @return mixed
+     * @throws BindingResolutionException
+     * @throws Exception
      */
     public function createByArray($items, array $options = [], array $data = [])
     {
@@ -106,14 +113,14 @@ class FormBuilder
     }
 
     /**
-     * @param $form
+     * @param Form  $form
      * @param $items
      */
-    public function buildFormByArray($form, $items)
+    public function buildFormByArray(Form $form, $items)
     {
         foreach ($items as $item) {
             if (!isset($item['name'])) {
-                throw new \InvalidArgumentException(
+                throw new InvalidArgumentException(
                     'Name is not set in form array.'
                 );
             }
@@ -161,7 +168,7 @@ class FormBuilder
     public function setFormClass($class) {
         $parent = Form::class;
         if (!is_a($class, $parent, true)) {
-            throw new \InvalidArgumentException("Class must be or extend $parent; $class is not.");
+            throw new InvalidArgumentException("Class must be or extend $parent; $class is not.");
         }
 
         $this->plainFormClass = $class;
@@ -170,9 +177,11 @@ class FormBuilder
     /**
      * Get instance of the empty form which can be modified.
      *
-     * @param array $options
-     * @param array $data
-     * @return \Kris\LaravelFormBuilder\Form
+     * @param array  $options
+     * @param array  $data
+     * @return Form
+     * @throws BindingResolutionException
+     * @throws Exception
      */
     public function plain(array $options = [], array $data = [])
     {
@@ -190,12 +199,13 @@ class FormBuilder
     }
 
     /**
-     * Set depedencies and options on existing form instance
+     * Set dependencies and options on existing form instance
      *
-     * @param \Kris\LaravelFormBuilder\Form $instance
-     * @param array $options
-     * @param array $data
-     * @return \Kris\LaravelFormBuilder\Form
+     * @param Form  $instance
+     * @param array  $options
+     * @param array  $data
+     * @return Form
+     * @throws BindingResolutionException
      */
     public function setDependenciesAndOptions($instance, array $options = [], array $data = [])
     {
